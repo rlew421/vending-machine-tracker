f2f6c9bc6e731fffa3e961fa2ff398bdad0caac7
Rachel Lew <rlew07@gmail.com>
2019-12-19 09:50:07 -0700

    Add test that a machine show page displays average price for all snacks in that machine

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
   it "I see the name of all the snacks associated with that vending machine along with their price" do
     owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    dons  = owner.machines.create(location: "Don's Super Awesome Vending Machine")
 
     hot_cheetos = dons.snacks.create!(name: "Hot Cheetos", price: 1.75)
     pringles = dons.snacks.create!(name: "Pringles", price: 2.50)
@@ -22,17 +22,30 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     within "#snack-#{hot_cheetos.id}" do
       expect(page).to have_content(hot_cheetos.name)
-      expect(page).to have_content(hot_cheetos.price)
+      expect(page).to have_content("Price: #{hot_cheetos.price}")
     end
 
     within "#snack-#{pringles.id}" do
       expect(page).to have_content(pringles.name)
-      expect(page).to have_content(pringles.price)
+      expect(page).to have_content("Price: #{pringles.price}")
     end
 
     within "#snack-#{sour_patch_kids.id}" do
       expect(page).to have_content(sour_patch_kids.name)
-      expect(page).to have_content(sour_patch_kids.price)
+      expect(page).to have_content("Price: #{sour_patch_kids.price}")
     end
+  end
+
+  it "I see an average price for all the snacks in that machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Super Awesome Vending Machine")
+
+    hot_cheetos = dons.snacks.create!(name: "Hot Cheetos", price: 150)
+    pringles = dons.snacks.create!(name: "Pringles", price: 250)
+    sour_patch_kids = dons.snacks.create!(name: "Sour Patch Kids", price: 200)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average Price: $2.00")
   end
 end
