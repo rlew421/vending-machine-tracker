77b35c63253b1d3351f341ff5ffc2a2edc98dede
Rachel Lew <rlew07@gmail.com>
2019-12-19 09:49:42 -0700

    Add model test and average_price instance method

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Snack, type: :model do
     it {should have_many :machine_snacks}
     it {should have_many(:machines).through(:machine_snacks)}
   end
+
+  describe 'instance methods' do
+    it "#average_price" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Super Awesome Vending Machine")
+
+      hot_cheetos = dons.snacks.create!(name: "Hot Cheetos", price: 150)
+      pringles = dons.snacks.create!(name: "Pringles", price: 250)
+      sour_patch_kids = dons.snacks.create!(name: "Sour Patch Kids", price: 200)
+
+      expect(dons.average_price).to eq(2.00)
+    end
+  end
 end
